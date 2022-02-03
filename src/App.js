cd666b831520a3461de28e423b7ef1604e403781
Kevin Castillo <kevincastillo318@gmail.com>
2022-02-03 14:45:16 -0800

    removed header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 
 export const App = () => {
   return (
     <div className="App ">
-      <Header />
       <Body />
       <Footer />
     </div>
